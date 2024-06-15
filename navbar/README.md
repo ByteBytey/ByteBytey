b329b47707c613091add5e2f50a55709bcaaa2eb
ByteBytey <162716900+ByteBytey@users.noreply.github.com>
2024-06-15 09:17:02 +0800

    Add files via upload
    this is too much files lmao

---
+++ navbar/README.md
@@ -0,0 +1,6 @@
+This Project is like my first project after
+a very long time, trying to focus on studying
+for my school and etc..
+
+idk how to make a readme properly its my first time
+using this lmao
---
+++ navbar/main_menu/main.js
@@ -0,0 +1,10 @@
+const product1 = document.getElementById("product1");
+const product2 = document.getElementById("product2");
+
+product1.onclick() = function(){
+    window.prompt("thank you for purchasing")
+}
+
+product2.onclick() = function(){
+    window.prompt("thank you for purchasing")
+}
