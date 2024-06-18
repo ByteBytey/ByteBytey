c3c7b26f17a9f99199953c11d24a44ad7ca90a04
ByteBytey <162716900+ByteBytey@users.noreply.github.com>
2024-06-18 14:02:15 +0800

    Add files via upload
    Made this for sm1

---
+++ sys/index.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("button");
+const Senior = "Senior Developer";
+getjunior.onclick() = function(){
+    let use = prompt("Are you sure you want to Hire a Junior?");
+}
